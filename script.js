2dbe774a19e65107de475b4bcb7efdfb406b6abe
itsmichaeld <67594883+itsmichaeld@users.noreply.github.com>
2023-01-24 15:27:05 +0000

    Added list add and remove functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// omdb api key: a5474722
 const searchBtn = document.getElementById("search-btn");
 const searchInput = document.getElementById("search-input");
 const filmList = document.getElementById("film-list");
+const initialPlaceholder = document.getElementById("initial-placeholder");
 let filmIdList = [];
 
 searchBtn.addEventListener("click", function (e) {
@@ -10,9 +10,12 @@ searchBtn.addEventListener("click", function (e) {
     .then((res) => res.json())
     .then((data) => {
       filmList.innerHTML = "";
+      initialPlaceholder.innerHTML = "";
+      filmList.style.padding = "2.75em";
       filmIdList = [];
+      noDataText();
       getFilmIds(data);
-      getSearchResultsList(filmIdList);
+      renderSearchResultsList(filmIdList);
     });
 });
 
@@ -23,39 +26,58 @@ function getFilmIds(data) {
   return filmIdList;
 }
 
-const getSearchResultsList = function (arr) {
+function noDataText() {
+  if (searchInput.value === "") {
+    return (filmList.innerHTML = `<section class="no-data">
+        <p class="no-data-text">Unable to find what you're looking for. Please try another search.
+        </p>
+      </section>`);
+  }
+}
+
+function add(movie) {
+  if (localStorage.getItem(`${movie.id}`)) {
+    return "";
+  } else {
+    localStorage.setItem(`${movie.id}`, `${movie.id}`);
+  }
+}
+
+const renderSearchResultsList = function (arr) {
   arr.forEach((id) => {
     fetch(`http://www.omdbapi.com/?apikey=a5474722&i=${id}`)
       .then((res) => res.json())
       .then((data) => {
         return (filmList.innerHTML += `<article class="film">
-          <img src="${data.Poster}" class="movie-poster" alt="${data.Title} poster" />
-          <div class="movie-details">
-            <div class="title-rating">
-              <h3 class="movie-title">${data.Title}</h3>
-              <img
-                src="./images/star-icon.png"
-                class="rating-star"
-                alt="star rating"
-              />
-              <p class="rating">${data.imdbRating}</p>
+            <img src="${data.Poster}" class="movie-poster" alt="${data.Title} poster" />
+            <div class="movie-details">
+              <div class="title-rating">
+                <h3 class="movie-title">${data.Title}</h3>
+                <img
+                  src="./images/star-icon.png"
+                  class="rating-star"
+                  alt="star rating"
+                />
+                <p class="rating">${data.imdbRating}</p>
+              </div>
+              <div class="movie-time-genre-add">
+                <p class="movie-time">${data.Runtime}</p>
+                <p class="movie-genre">${data.Genre}</p>
+                <a href="#" class="watchlist-add-link" onclick="add(${data.imdbID})" id="${data.imdbID}">
+                <img
+                  src="./images/plus-icon.png"
+                  alt="add to watchlist"
+                  class="add-icon"
+                />
+                <p>Watchlist</p>
+                </a>
+              </div>
+              <p class="movie-description">
+                ${data.Plot}
+              </p>
             </div>
-            <div class="movie-time-genre-add">
-              <p class="movie-time">${data.Runtime}</p>
-              <p class="movie-genre">${data.Genre}</p>
-              <img
-                src="./images/plus-icon.png"
-                alt="add to watchlist"
-                class="add-icon"
-              />
-              <p>Watchlist</p>
-            </div>
-            <p class="movie-description">
-              ${data.Plot}
-            </p>
-          </div>
-        </article>
-        <hr class="film-divider" />`);
+          </article>
+          <hr class="film-divider" />`);
       });
   });
 };
---
+++ watchList.js
@@ -0,0 +1,74 @@
+const watchlist = document.getElementById("watchlist");
+const emptyWatchlistText = document.getElementById("empty-watchlist");
+let filmsArray = { ...localStorage };
+
+window.addEventListener("load", (e) => {
+  watchlist.style.padding = "2.75em";
+  renderWatchList(filmsArray);
+  emptyWatchlist();
+});
+
+function remove(movie) {
+  localStorage.removeItem(`${movie.id}`);
+  filmsArray = { ...localStorage };
+  renderWatchList(filmsArray);
+  emptyWatchlist();
+}
+
+function emptyWatchlist() {
+  if (localStorage.length === 0) {
+    watchlist.style.padding = "0";
+    emptyWatchlistText.innerHTML = `<h3 class="empty-watchlist-header">
+      Your watchlist is looking a little empty...
+    </h3>
+    <a href="index.html" class="add-to-list-link">
+      <img
+        src="./images/plus-icon.png"
+        alt="add to watchlist"
+        class="add-icon"
+      />
+      <p class="empty-list-text">Let's add some movies</p>
+    </a>`;
+  }
+}
+
+const renderWatchList = function (arr) {
+  watchlist.innerHTML = "";
+  Object.values(arr).forEach((id) => {
+    fetch(`http://www.omdbapi.com/?apikey=a5474722&i=${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        return (watchlist.innerHTML += `<article class="film">
+              <img src="${data.Poster}" class="movie-poster" alt="${data.Title} poster" />
+              <div class="movie-details">
+                <div class="title-rating">
+                  <h3 class="movie-title">${data.Title}</h3>
+                  <img
+                    src="./images/star-icon.png"
+                    class="rating-star"
+                    alt="star rating"
+                  />
+                  <p class="rating">${data.imdbRating}</p>
+                </div>
+                <div class="movie-time-genre-add">
+                  <p class="movie-time">${data.Runtime}</p>
+                  <p class="movie-genre">${data.Genre}</p>
+                  <a href="#" class="watchlist-add-link"
+                  onclick="remove(${data.imdbID})" id="${data.imdbID}">
+                  <img
+                    src="./images/minus-icon.png"
+                    alt="remove from watchlist"
+                    class="remove-icon"
+                  />
+                  <p>Watchlist</p>
+                  </a>
+                </div>
+                <p class="movie-description">
+                  ${data.Plot}
+                </p>
+              </div>
+            </article>
+            <hr class="film-divider" />`);
+      });
+  });
+};
